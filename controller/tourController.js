4dfd3ffeb2d6f1ad44d896d2235440be4bf01a97
Jitisha Khede <khedejitisha2004@gmail.com>
2024-08-02 16:11:43 +0530

    started building natous

---
+++ controller/tourController.js
@@ -0,0 +1,71 @@
+const Tour = require('./../model/tourModel');
+//const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`));
+
+
+exports.getAllTours = async (req,res) =>{
+
+    const tours  = await Tour.find();
+
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data : {
+            tours
+        }
+    });
+};
+exports.getTour = async (req,res)=>{
+
+    try{
+        const tour = await Tour.findById(req.params.id);
+
+        res.status(200).json({
+            status : 'success',
+            data: {
+                tour
+            }
+        });
+    }
+    catch(err){
+        res.status(400).json({
+            status: 'fail',
+            message : 'invalid data sent!'
+        })
+    }
+}
+
+exports.createTour = async (req,res) =>{
+    try{
+        const newTour = await Tour.create(req.body);
+
+    res.status(201).json({
+        status:'success',
+        data :{
+            tour : newTour
+        }
+    });
+    }catch(err){
+        res.status(400).json({
+            status: 'fail',
+            message : 'invalid data sent!'
+        })
+    }
+    
+
+}
+exports.updateTour = (req,res) =>{
+    
+    res.status(200).json({
+        status:'success',
+        data: {
+            tour:'<Updated tour here...>'
+        }
+    });
+}
+exports.deleteTour = (req,res) =>{
+
+    res.status(204).json({
+        status:'success',
+        data: null
+    });
+}
