9e30c97c40f32784dfe7019acb669b0a4fa6b29f
Jitisha Khede <khedejitisha2004@gmail.com>
2024-08-09 23:17:57 +0530

    updated tour model

--- controller/tourController.js
+++ controller/tourController.js
@@ -29,7 +29,7 @@ exports.getTour = async (req,res)=>{
     catch(err){
         res.status(400).json({
             status: 'fail',
-            message : 'invalid data sent!'
+            message : err
         })
     }
 }
@@ -47,7 +47,7 @@ exports.createTour = async (req,res) =>{
     }catch(err){
         res.status(400).json({
             status: 'fail',
-            message : 'invalid data sent!'
+            message : err
         })
     }
     
