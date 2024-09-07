384e106f8b9263a00721d609ffebc4beb0406fd2
Jitisha Khede <khedejitisha2004@gmail.com>
2024-09-08 00:55:04 +0530

    added 404 not found error handler

--- controller/tourController.js
+++ controller/tourController.js
@@ -58,7 +58,11 @@ exports.updateTour =catchAsync( async (req,res,next) =>{
     const tour = await Tour.findByIdAndUpdate(req.params.id,req.body,{
         new: true,
         runValidators: true
-    })
+    });
+
+    if(!tour) {
+        return next(new AppError('No tour found with that ID',404));
+    }
     
     res.status(200).json({
         status:'success',
@@ -71,7 +75,11 @@ exports.updateTour =catchAsync( async (req,res,next) =>{
 
 exports.deleteTour = catchAsync (async (req,res,next) =>{
 
-    await Tour.findByIdAndDelete(req.params.id);
+    const tour = await Tour.findByIdAndDelete(req.params.id);
+    
+    if(!tour) {
+        return next(new AppError('No tour found with that ID',404));
+    }
 
     res.status(204).json({
         status:'success',
