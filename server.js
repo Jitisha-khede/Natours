70b5d42cffc0ce0579295539baf7d876d78597eb
Jitisha Khede <khedejitisha2004@gmail.com>
2024-09-10 09:10:48 +0530

    handeling unhandeled promises

--- server.js
+++ server.js
@@ -14,6 +14,12 @@ const DB = process.env.DATABASE.replace('<PASSWORD>',process.env.DATABASE_PASSWO
 
 
 const port = process.env.PORT || 3000;
-app.listen(port,()=>{
+const server = app.listen(port,()=>{
     console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', err=> {
+    console.log(err.name, err.message);
+    console.log('UNHANDELED REJECTIONS, shutting down...');
+    server.close(() => process.exit(1));
 });
