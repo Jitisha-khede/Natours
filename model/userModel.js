7d13ec8010088e5c5a4e592d19226916bc676dd2
Jitisha Khede <khedejitisha2004@gmail.com>
2024-09-11 09:26:16 +0530

    password management

--- model/userModel.js
+++ model/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -21,8 +22,26 @@ const userSchema = new mongoose.Schema({
     },
     passwordConfirm: {
         type: String,
-        required: [true, 'Please confirm your password']
+        required: [true, 'Please confirm your password'],
+        validate:{
+            validator: function(el){
+                return el === this.password;
+            },
+            message: 'Passwords are not the same!'
+        }
     }
+});
+
+userSchema.pre('save', async function(next){
+    //only runnthis function if password is actually modified
+    if(!this.isModified('password')) return next();
+
+    //hash the password with the cost of 12
+    this.password = await bcrypt.hash(this.password,12);
+
+    //delete the passwordConfirm field
+    this.passwordConfirm = undefined;
+    next();
 });
 
 const User = mongoose.model('User', userSchema);
