f4e8c5bef4b1520f566e05d74485ba328209d093
Jitisha Khede <khedejitisha2004@gmail.com>
2024-09-13 16:00:06 +0530

    logging in user

--- controller/authController.js
+++ controller/authController.js
@@ -1,5 +1,13 @@
+const jwt = require('jsonwebtoken')
 const User = require('./../model/userModel');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+const signToken = id => {
+    return jwt.sign({ id } , process.env.JWT_SECREAT,{
+        expiresIn: process.env.JWT_EXPIRES_IN
+    });
+}
 
 exports.signup = catchAsync( async (req,res,next) => {
     const newUser = await User.create({
@@ -9,10 +17,41 @@ exports.signup = catchAsync( async (req,res,next) => {
         passwordConfirm: req.body.passwordConfirm
     });
 
+    const token = signToken(newUser._id);
     res.status(201).json({
         status: 'success',
+        token,
         data: {
             user: newUser
         }
+    });
+});
+exports.login = catchAsync(async (req, res, next) => {
+    const { email, password } = req.body;
+
+    // 1) Check if email and password exist
+    if (!email || !password) {
+        return next(new AppError('Please provide email and password', 400)); // 400 for bad request
+    }
+
+    // 2) Check if user exists and password is correct
+    const user = await User.findOne({email}).select('+password');
+
+    if (!user) {
+        return next(new AppError('Incorrect email or password', 401)); // 401 for unauthorized
+    }
+
+    const correct = await user.correctPassword(password, user.password);
+
+    if (!correct) {
+        return next(new AppError('Incorrect email or password', 401));
+    }
+
+    // 3) If everything is okay, send the token to the client
+    const token = signToken(user._id);
+    console.log(token);
+    res.status(200).json({
+        status: 'success',
+        token
     });
 });
--- controller/userController.js
+++ controller/userController.js
@@ -1,3 +1,6 @@
+const User = require("../model/userModel");
+const catchAsync = require("../utils/catchAsync");
+
 exports.getUser = (req,res) =>{
     res.status(500).json({
         status: 'error',
@@ -10,12 +13,18 @@ exports.updateUser = (req,res) =>{
         message: 'This route is not defined yet'
     })
 }
-exports.getAllUsers = (req,res) =>{
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not defined yet'
-    })
-}
+exports.getAllUsers = catchAsync( async(req,res) =>{
+    const users = await User.find();
+
+    //SEND RESPONSE
+    res.status(200).json({
+        status: 'success',
+        results: User.length,
+        data : {
+            users
+        }
+    });
+})
 exports.deleteUser = (req,res) =>{
     res.status(500).json({
         status: 'error',
